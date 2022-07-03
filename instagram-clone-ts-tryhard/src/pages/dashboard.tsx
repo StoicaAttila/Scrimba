b84fd894da890758f91a0bd5f9880a0f01a6caac
StoicaAttila <stoicaattila99@gmail.com>
2022-07-03 19:26:49 +0300

    Instagram Clone TS TryHard
    not-found page doesn't work for some reason...

--- instagram-clone-ts-tryhard/src/pages/dashboard.tsx
+++ instagram-clone-ts-tryhard/src/pages/dashboard.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Header from '../components/header';
+import Timeline from '../components/timeline';
+import Sidebar from '../components/sidebar/index';
 
 export default function Dashboard() {
+
+    React.useEffect(() => {
+        document.title = 'Instagram';
+    }, []);
+
     return (
-        <p>Hello from Dashboard</p>
+        <div className="bg-gray-100">
+            <Header />
+            <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+                <Timeline />
+                <Sidebar />
+            </div>
+        </div>
     )
 }
--- instagram-clone-ts-tryhard/src/pages/notfound.tsx
+++ instagram-clone-ts-tryhard/src/pages/notfound.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Header from '../components/header';
 
 export default function NotFound() {
+
+    React.useEffect(() => {
+        document.title = '404 - Not Found';
+    }, []);
+
     return (
-        <p>Hello from NotFound</p>
+        <div className="bg-gray">
+            <Header />
+            <div className="mx-auto max-w-screen-lg">
+                <p className="text-center text-2xl">Not Found!</p>
+            </div>
+        </div>
     )
 }
